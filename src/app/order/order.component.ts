5e5618d75c3fe654be7ac38432a048c345846f4c
Leandro <leandro.martins1912@gmail.com>
2020-07-11 18:45:59 -0300

    Delivery-coats, shopping Cart, Order, Componente radio

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from '../shared/radio/radio-option.model';
+import { OrderService } from './order.service'
+import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 
 @Component({
   selector: 'mt-order',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
-  constructor() { }
+  delivery: number = 8
+
+  paymentOptions: RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartão de Débito', value: 'DEB'},
+    {label: 'Cartão de Refeição', value: 'REF'}
+  ]
+
+  constructor(private orderService: OrderService) { }
 
   ngOnInit() {
+  }
+
+  itemsValue(): number {
+    return this.orderService.itemsValue()
+  }
+
+  cartItems(): CartItem[]{
+    return this.orderService.cartItem()
+  }
+
+  increaseQty(item: CartItem){
+    return this.orderService.increaseQty(item)
+  }
+
+  decreaseQty(item: CartItem){
+    return this.orderService.decreaseQty(item)
+  }
+
+  remove(item: CartItem){
+    return this.orderService.removeItem(item)
   }
 
 }
